11fce5aec82cc5d50543f2492daf0aeecef4c1da
M4tthewDE <mattider2@gmail.com>
2021-12-26 14:41:41 +0100

    add username and channel to command

--- src/main/java/de/com/fdm/bot/Command.java
+++ src/main/java/de/com/fdm/bot/Command.java
@@ -7,10 +7,14 @@ import java.util.List;
 
 public class Command {
     private final String identifier;
+    private final String channel;
+    private final String user;
     private final List<String> args;
 
-    public Command(String identifier, List<String> args) {
+    public Command(String identifier, String channel, String user, List<String> args) {
         this.identifier = identifier;
+        this.channel = channel;
+        this.user = user;
         this.args = args;
     }
 
@@ -20,13 +24,15 @@ public class Command {
         String identifier = chunks[0];
         List<String> args = Arrays.stream(chunks).toList().subList(1, chunks.length);
 
-        return new Command(identifier, args);
+        return new Command(identifier, msg.getChannel(), msg.getName(), args);
     }
 
     @Override
     public String toString() {
         return "Command{" +
                 "identifier='" + identifier + '\'' +
+                ", channel='" + channel + '\'' +
+                ", user='" + user + '\'' +
                 ", args=" + args +
                 '}';
     }
