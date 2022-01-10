54311227fd25653af9b4469dc84d756aa56a45b3
M4tthewDE <mattider2@gmail.com>
2022-01-10 15:58:25 +0100

    add ratelimiting to follow alerts

--- src/main/java/de/com/fdm/grpc/microsub/consumer/MicrosubConsumerServiceImpl.java
+++ src/main/java/de/com/fdm/grpc/microsub/consumer/MicrosubConsumerServiceImpl.java
@@ -14,10 +14,14 @@ import io.grpc.stub.StreamObserver;
 import net.devh.boot.grpc.server.service.GrpcService;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import java.time.Instant;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @GrpcService
 public class MicrosubConsumerServiceImpl extends ConsumerGrpc.ConsumerImplBase {
+    private final Map<String, Instant> userLastAlertTime;
 
     @Autowired
     private DispatcherClient dispatcherClient;
@@ -27,6 +31,10 @@ public class MicrosubConsumerServiceImpl extends ConsumerGrpc.ConsumerImplBase {
 
     @Autowired
     private MicroSubRepository microSubRepository;
+
+    public MicrosubConsumerServiceImpl() {
+        this.userLastAlertTime = new HashMap<>();
+    }
 
     @Override
     public void consume(EventsubMessage eventsubMessage, StreamObserver<Empty> responseOvserver) {
@@ -67,12 +75,10 @@ public class MicrosubConsumerServiceImpl extends ConsumerGrpc.ConsumerImplBase {
     }
 
     private void sendTurtoiseFollowAlert(EventsubMessage eventsubMessage) {
-        String text;
-        if (eventsubMessage.getIsGift()) {
-            text = String.format("PagChomp OH SHIT %s YOU JUST GOT GIFTED", eventsubMessage.getEventUserName());
-        } else {
-            text = String.format("PagChomp OH SHIT %s THANKS FOR THE FOLLOW", eventsubMessage.getEventUserName());
+        if (!canSendFollow(eventsubMessage)) {
+            return;
         }
+        String text = String.format("PagChomp OH SHIT %s THANKS FOR THE FOLLOW", eventsubMessage.getEventUserName());
 
         OutboundMessage msg = OutboundMessage.newBuilder()
                 .setText(text)
@@ -84,7 +90,12 @@ public class MicrosubConsumerServiceImpl extends ConsumerGrpc.ConsumerImplBase {
     }
 
     private void sendTurtoiseSubAlert(EventsubMessage eventsubMessage) {
-        String text = String.format("heCrazy YOOO %s THANKS FOR SUBBING", eventsubMessage.getEventUserName());
+        String text;
+        if (eventsubMessage.getIsGift()) {
+            text = String.format("heCrazy YOOO %s THANKS FOR SUBBING", eventsubMessage.getEventUserName());
+        } else {
+            text = String.format("PagChomp OH SHIT %s YOU JUST GOT GIFTED", eventsubMessage.getEventUserName());
+        }
         OutboundMessage msg = OutboundMessage.newBuilder()
                 .setText(text)
                 .setChannel("turtoise")
@@ -92,5 +103,21 @@ public class MicrosubConsumerServiceImpl extends ConsumerGrpc.ConsumerImplBase {
                 .build();
 
         dispatcherClient.send(msg);
+    }
+
+    private boolean canSendFollow(EventsubMessage msg) {
+        if (!userLastAlertTime.containsKey(msg.getEventUserId())) {
+            userLastAlertTime.put(msg.getEventUserId(), Instant.now());
+            return true;
+        }
+
+        long lastAlertTime = userLastAlertTime.get(msg.getEventUserId()).getEpochSecond();
+
+        if ((Instant.now().getEpochSecond() - lastAlertTime) >= 60) {
+            userLastAlertTime.put(msg.getEventUserId(), Instant.now());
+            return true;
+        }
+
+        return false;
     }
 }
