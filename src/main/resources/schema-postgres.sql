9be0bb35fe916e4e5c9dfeece4303d620dfd429a
M4tthewDE <mattider2@gmail.com>
2022-04-24 22:02:11 +0200

    Remove database schema

--- src/main/resources/schema-postgres.sql
+++
@@ -1,8 +0,0 @@
-DROP TABLE IF EXISTS microsub;
-
-
-CREATE TABLE microsub (
-    microsub_id serial PRIMARY KEY,
-    channel VARCHAR(255) NOT NULL,
-    broadcaster_user_id VARCHAR(255) NOT NULL
-);
