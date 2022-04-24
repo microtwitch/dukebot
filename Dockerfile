73df5baade982306e0272c7fc95543311181c7aa
M4tthewDE <mattider2@gmail.com>
2022-04-24 13:56:51 +0200

    pass commit hash via file

--- Dockerfile
+++ Dockerfile
@@ -4,10 +4,12 @@ COPY settings.gradle .
 COPY build.gradle .
 COPY gradle gradle
 COPY src src
+COPY .git .git
 
+RUN git rev-parse --short HEAD >> commit.txt
 RUN gradle build
 
 FROM bellsoft/liberica-openjdk-alpine:latest
-RUN export GIT_COMMIT=$(git rev-parse --short HEAD)
+COPY --from=builder /home/gradle/commit.txt commit.txt
 COPY --from=builder /home/gradle/build/libs/*.jar app.jar
 ENTRYPOINT ["java", "-jar", "/app.jar"]
--- src/main/java/de/com/fdm/bot/commands/CommitCommand.java
+++ src/main/java/de/com/fdm/bot/commands/CommitCommand.java
@@ -4,12 +4,27 @@ import de.com.fdm.bot.Command;
 import de.com.fdm.bot.Parameters;
 import org.springframework.stereotype.Component;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
 @Component
 public class CommitCommand implements Command {
     @Override
     public String execute(Parameters params) {
-        System.out.println(System.getenv());
+        return getCommit();
+    }
 
-        return System.getenv("GIT_COMMIT");
+    private String getCommit() {
+        Path filePath = Path.of("commit.txt");
+        String commit;
+        try {
+            byte[] bytes = Files.readAllBytes(filePath);
+            commit = new String (bytes);
+        } catch (IOException e) {
+            return "Error reading commit";
+        }
+
+        return commit;
     }
 }
