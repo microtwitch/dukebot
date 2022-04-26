535ec7c589ba3de3788399cff323772b7d049be3
M4tthewDE <mattider2@gmail.com>
2022-04-26 22:19:41 +0200

    convert TmiService to kotlin

--- src/main/java/de/com/fdm/twitch/tmi/TmiService.kt
+++ src/main/java/de/com/fdm/twitch/tmi/TmiService.kt
@@ -1,83 +1,61 @@
-package de.com.fdm.twitch.tmi;
+package de.com.fdm.twitch.tmi
 
-import com.github.philippheuer.credentialmanager.domain.OAuth2Credential;
-import com.github.twitch4j.TwitchClient;
-import com.github.twitch4j.TwitchClientBuilder;
-import com.github.twitch4j.chat.events.channel.ChannelMessageActionEvent;
-import com.github.twitch4j.chat.events.channel.ChannelMessageEvent;
-import de.com.fdm.bot.api.haste.HasteService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Service;
-
-import java.util.Set;
-import java.util.function.Consumer;
+import com.github.philippheuer.credentialmanager.domain.OAuth2Credential
+import com.github.twitch4j.TwitchClient
+import com.github.twitch4j.TwitchClientBuilder
+import com.github.twitch4j.chat.events.channel.ChannelMessageEvent
+import de.com.fdm.bot.api.haste.HasteService
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.stereotype.Service
+import java.util.function.Consumer
 
 @Service
-public class TmiService {
-    private final TwitchClient client;
-    private final HasteService hasteService;
-    private Consumer<TmiMessage> callback;
+class TmiService(
+        @Value("\${bot.auth}") botAuth: String,
+        @param:Autowired private val hasteService: HasteService
+) {
+    private val client: TwitchClient
 
-    public TmiService(
-            @Value("${bot.auth}") String botAuth,
-            @Autowired HasteService hasteService
-    ) {
-        this.hasteService = hasteService;
-
-        OAuth2Credential credentials = new OAuth2Credential("twitch", botAuth);
-        this.client = TwitchClientBuilder.builder()
+    init {
+        val credentials = OAuth2Credential("twitch", botAuth)
+        client = TwitchClientBuilder.builder()
                 .withEnableChat(true)
                 .withChatAccount(credentials)
-                .build();
-
+                .build()
     }
 
-    public void setCallback(Consumer<TmiMessage> callback) {
-        this.callback = callback;
-
-        client.getEventManager().onEvent(
-                ChannelMessageActionEvent.class, this::actionMessageCallback
-        );
-
-        client.getEventManager().onEvent(
-                ChannelMessageEvent.class, this::messageCallback
-        );
-    }
-
-    private void messageCallback(ChannelMessageEvent event) {
-        this.callback.accept(new TmiMessage(event));
-    }
-
-    private void actionMessageCallback(ChannelMessageActionEvent event) {
-        this.callback.accept(new TmiMessage(event));
-    }
-
-    public void send(String channel, String msg) {
-        if (msg.length() >= 500) {
-            String url = hasteService.upload(msg);
-            client.getChat().sendMessage(channel, url);
-            return;
-        }
-
-        client.getChat().sendMessage(channel, msg);
-    }
-
-    public void joinChannels(String[] channels) {
-        for (String channel : channels) {
-            client.getChat().joinChannel(channel);
+    fun setCallback(callback: Consumer<TmiMessage>) {
+        client.eventManager.onEvent(ChannelMessageEvent::class.java) {
+            event: ChannelMessageEvent -> callback.accept(TmiMessage(event))
         }
     }
 
-    public void joinChannel(String channel) {
-        client.getChat().joinChannel(channel);
+    fun send(channel: String?, msg: String) {
+        if (msg.length >= 500) {
+            val url = hasteService.upload(msg)
+            client.chat.sendMessage(channel, url)
+            return
+        }
+
+        client.chat.sendMessage(channel, msg)
     }
 
-    public void partChannel(String channel) {
-        client.getChat().leaveChannel(channel);
+    fun joinChannels(channels: Array<String?>) {
+        for (channel in channels) {
+            client.chat.joinChannel(channel)
+        }
     }
 
-    public Set<String> getChannels() {
-        return client.getChat().getChannels();
+    fun joinChannel(channel: String?) {
+        client.chat.joinChannel(channel)
+    }
+
+    fun partChannel(channel: String?) {
+        client.chat.leaveChannel(channel)
+    }
+
+    fun getChannels() : Set<String> {
+        return client.chat.channels
     }
 }
