9127c5fa2f219da3646def6931727072e38e02c1
M4tthewDE <mattider2@gmail.com>
2022-04-26 16:05:24 +0200

    Add option to pipe any command to hastebin

--- src/main/java/de/com/fdm/bot/MessageHandler.java
+++ src/main/java/de/com/fdm/bot/MessageHandler.java
@@ -1,5 +1,6 @@
 package de.com.fdm.bot;
 
+import de.com.fdm.bot.api.haste.HasteService;
 import de.com.fdm.twitch.tmi.TmiMessage;
 import de.com.fdm.twitch.tmi.TmiService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,15 +11,18 @@ public class MessageHandler {
     private final PermissionsService permissionsService;
     private final TmiService tmiService;
     private final CommandRunner commandRunner;
+    private final HasteService hasteService;
 
     public MessageHandler(
             @Autowired PermissionsService permissionsService,
             @Autowired TmiService tmiService,
-            @Autowired CommandRunner commandRunner
+            @Autowired CommandRunner commandRunner,
+            @Autowired HasteService hasteService
     ) {
         this.permissionsService = permissionsService;
         this.tmiService = tmiService;
         this.commandRunner = commandRunner;
+        this.hasteService = hasteService;
 
         tmiService.setCallback(this::handleMessage);
     }
@@ -29,6 +33,13 @@ public class MessageHandler {
         }
 
         String result = this.commandRunner.runCommand(msg);
+
+        if (msg.getMessage().strip().endsWith("| haste")) {
+            String haste = hasteService.upload(result);
+            tmiService.send(msg.getChannel(), haste);
+            return;
+        }
+
         tmiService.send(msg.getChannel(), result);
     }
 }
